f24acb18122c8cd2176ea53edcb74383305b48b5
Dmitry Rogatsevich <Dmitry.Rogatsevich@oropay.com>
2020-02-28 16:35:52 +0300

    test ML

--- TestML/ViewController.swift
+++ TestML/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import Vision
+import CoreML
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: ImageClassifier().model)
+            else { fatalError("Can't load VisionML model") }
 
+        let request = VNCoreMLRequest(model: visionModel) { (request, error) in
+            guard let results = request.results else { return }
+            self.handleRequestResults(results)
+        }
+
+        requests = [request]
+    }
+    
+    func handleRequestResults(_ results: [Any]) {
+        let categoryText: String?
+
+        defer {
+            DispatchQueue.main.async {
+                self.categoryLabel.text = categoryText
+            }
+        }
+
+        guard let foundObject = results
+            .compactMap({ $0 as? VNClassificationObservation })
+            .first(where: { $0.confidence > 0.7 })
+            else {
+                categoryText = nil
+                return
+        }
+
+        let category = categoryTitle(identifier: foundObject.identifier)
+        let confidence = "\(round(foundObject.confidence * 100 * 100) / 100)%"
+        categoryText = "\(category) \(confidence)"
+    }
+    
+    func captureOutput(
+        _ output: AVCaptureOutput,
+        didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+
+        var requestOptions: [VNImageOption: Any] = [:]
+        if let cameraIntrinsicData = CMGetAttachment(
+            sampleBuffer,
+            key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix,
+            attachmentModeOut: nil) {
+            requestOptions = [.cameraIntrinsics:cameraIntrinsicData]
+        }
+        let imageRequestHandler = VNImageRequestHandler(
+            cvPixelBuffer: pixelBuffer,
+            options: requestOptions)
+
+        do {
+            try imageRequestHandler.perform(requests)
+        } catch {
+            print(error)
+        }
+    }
 
 }
 
