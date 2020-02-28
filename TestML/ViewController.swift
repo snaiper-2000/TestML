cda7b3f922ce4bb3659264910d60f0406059abc7
Dmitry Rogatsevich <Dmitry.Rogatsevich@oropay.com>
2020-02-28 15:57:05 +0300

    Initial Commit

---
+++ TestML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestML
+//
+//  Created by Rogatsevich Dmitry on 2/28/20.
+//  Copyright © 2020 Rogatsevich Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
